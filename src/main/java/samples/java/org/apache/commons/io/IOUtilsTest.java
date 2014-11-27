3b09b021e2e9de82b9b3c92a9c10c595ea1ec2b7
izeye <izeye@naver.com>
2014-11-28 08:00:17 +0900

    Add an Apache Commons IO IOUtils sample .

---
+++ src/main/java/samples/java/org/apache/commons/io/IOUtilsTest.java
@@ -0,0 +1,21 @@
+package samples.java.org.apache.commons.io;
+
+import org.apache.commons.io.IOUtils;
+import org.junit.Test;
+
+import java.io.FileReader;
+import java.io.IOException;
+
+/**
+ * Created by izeye on 14. 11. 28..
+ */
+public class IOUtilsTest {
+
+    @Test
+    public void testToString() throws IOException {
+        String filename = "src/main/java/samples/java/org/apache/commons/io/test.txt";
+        String string = IOUtils.toString(new FileReader(filename));
+        System.out.println(string);
+    }
+
+}
