df58373e52b9644a985d7751567d4c2540044d8d
kr14652 <jungmuk.lim@navercorp.com>
2014-12-11 11:33:06 +0900

    Add a sample Java source for examining ASM.

---
+++ src/main/java/samples/java/asm/PrintTest.java
@@ -0,0 +1,15 @@
+package samples.java.asm;
+
+/**
+ * Created by nbp on 2014-12-11.
+ */
+public class PrintTest {
+
+  public static void main(String[] args) {
+    int i = 0;
+    System.out.println("i = " + i);
+    i++;
+    System.out.println("i = " + i);
+  }
+
+}
