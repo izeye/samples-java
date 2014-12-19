afc8389c4c660008d7b9cf9593c6d70eb06584ca
izeye <izeye@naver.com>
2014-12-20 08:12:05 +0900

    Add a test for Integer.

---
+++ src/main/java/samples/java/java/lang/IntegerTests.java
@@ -0,0 +1,16 @@
+package samples.java.java.lang;
+
+import org.junit.Test;
+
+/**
+ * Created by izeye on 14. 12. 20..
+ */
+public class IntegerTests {
+
+  @Test
+  public void testMinMax() {
+    System.out.println(Integer.MIN_VALUE);
+    System.out.println(Integer.MAX_VALUE);
+  }
+
+}
